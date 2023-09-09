91e14a8ce2044acf2f7a5b50d63df5a076f345da
blockchainGuru125 <kkh0102www@outlook.com>
2023-09-10 02:04:04 +1000

    fix

--- src/config/constants/testnetfarms.ts
+++ src/config/constants/testnetfarms.ts
@@ -15,58 +15,70 @@ const farms: SerializedFarmConfig[] = [
     pid: 0,
     lpSymbol: 'STEALTH',
     lpAddresses: {
-      8453: '0xC419328A3A5b7b4E2312946F90cd034Cf7abf0Cc',
-      1: '0x30885515b9AeCc599Dc6D48106B471EAd26dEBB0',
-      5: '',
+      8453: '0xC16f056D4eBb5246CfB3a1D128A1974fC2cec6Aa',
+      1: '0xB18F98822C22492Bd6b77D19cae9367f3D60fcBf',
+      5: '0x1Cd327b11467dF6766cf6FeaD879A15D7F869247'
     },
     token: serializedTokens.cake,
     quoteToken: serializedTokens.usdt,
-    isTokenOnly: true,
+    isTokenOnly: true
   },
   {
     pid: 1,
-    lpSymbol: 'STEALTH-USDT LP',
-    lpAddresses: {
-      8453: '0x8bfD8Fd94342ABDc404c3b780AbAf8bcF74f9E51',
-      1: '0x30885515b9AeCc599Dc6D48106B471EAd26dEBB0',
-      5: '',
-    },
-    token: serializedTokens.cake,
-    quoteToken: serializedTokens.usdt,
-  },
-  {
-    pid: 2,
     lpSymbol: 'STEALTH-ETH LP',
     lpAddresses: {
-      8453: '0xF7a7326F25Afb7da7b84D426B2e7824D326CDa2C',
+      8453: '0xf4929D6c5bBBC2f2ef8C32FAed7ddA80924E74de',
       1: CAKE_BNB_LP_MAINNET,
-      5: '',
+      5: ''
     },
     token: serializedTokens.cake,
     quoteToken: serializedTokens.wbnb,
   },
   {
-    pid: 3,
-    lpSymbol: 'USDT-ETH LP',
+    pid: 2,
+    lpSymbol: 'STEALTH-USDC LP',
     lpAddresses: {
-      8453: '0xa605851B07a2bb1883687fD2805b46fc218d7D92',
+      8453: '0x8c5905c63d94f4d841d4f1d400422deac7678fc7',
+      1: '0x30885515b9AeCc599Dc6D48106B471EAd26dEBB0',
+      5: ''
+    },
+    token: serializedTokens.cake,
+    quoteToken: serializedTokens.usdt,
+  },
+  {
+    pid: 3,
+    lpSymbol: 'USDC-ETH LP',
+    lpAddresses: {
+      8453: '0x1D3Dd7fCB2eC13a639E6B5265e63D9120e639444',
       1: '0xA8f8B7C0a4ec1ca9fA115dAe915e33AEDdf2526B',
-      5: '',
+      5: ''
     },
     token: serializedTokens.usdt,
     quoteToken: serializedTokens.wbnb,
   },
   {
-    pid: 5,
+    pid: 4,
     lpSymbol: 'WETH',
     lpAddresses: {
-      8453: '0x5fe8bBD2fD4Ee98b45a996DA2fd22263baD7A597',
+      8453: '0x4200000000000000000000000000000000000006',
       1: '0xA8f8B7C0a4ec1ca9fA115dAe915e33AEDdf2526B',
-      5: '',
+      5: ''
     },
     token: serializedTokens.wbnb,
     quoteToken: serializedTokens.usdt,
-    isTokenOnly: true,
+    isTokenOnly: true
+  },
+  {
+    pid: 5,
+    lpSymbol: 'USDC',
+    lpAddresses: {
+      8453: '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA',
+      1: '0xA8f8B7C0a4ec1ca9fA115dAe915e33AEDdf2526B',
+      5: ''
+    },
+    token: serializedTokens.usdt,
+    quoteToken: serializedTokens.usdt,
+    isTokenOnly: true
   },
 ]
 
